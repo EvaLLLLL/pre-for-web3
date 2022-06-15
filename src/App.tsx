acf2e6d678f20783e693e3f8977aaa3e8943e971
lyhhh <17610603221@163.com>
2022-06-15 21:10:23 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Web3 from 'web3'
+import './App.css'
 
 function App() {
+  React.useEffect(() => {
+    ;(window as any).ethereum.enable()
+  }, [])
+
+  const sendEther = async () => {
+    const web3 = new Web3((window as any).web3.currentProvider)
+    const [account] = await web3.eth.getAccounts()
+    console.log(account)
+
+    let transaction
+    try {
+      transaction = await web3.eth.sendTransaction({
+        from: account,
+        to: '',
+        value: '',
+      })
+      console.log('transaction', transaction)
+    } catch (err) {
+      console.log('err', err)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={sendEther}>发起交易</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
