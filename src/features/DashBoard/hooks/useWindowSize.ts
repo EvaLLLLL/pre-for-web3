e8cb0dfae4d3567a8baac4c5ddd41abc83d2c631
lyhhh <17610603221@163.com>
2022-06-18 11:54:46 +0800

    add dashboard ui layout

---
+++ src/features/DashBoard/hooks/useWindowSize.ts
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const useWindowSize = () => {
+  const [windowSize, setWindowSize] = React.useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  })
+
+  React.useEffect(() => {
+    const updateSize = () =>
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      })
+
+    window.addEventListener('resize', updateSize)
+    return () => window.removeEventListener('resize', updateSize)
+  }, [])
+
+  return windowSize
+}
--- src/features/DashBoard/index.tsx
+++ src/features/DashBoard/index.tsx
@@ -1,5 +1,27 @@
 import React from 'react'
+import styles from './index.module.scss'
+import { DataItem } from './components/DataItem'
+import { useWindowSize } from './hooks/useWindowSize'
 
 export const DashBoard: React.FC = () => {
-  return <div>dashboard</div>
+  const { width } = useWindowSize()
+  return (
+    <div className={styles.container}>
+      <div className={styles.device}>window width: {width}</div>
+
+      <div className={styles.content}>
+        <div className={styles.title}>Oracle</div>
+        <div className={styles.list}>
+          <DataItem />
+          <DataItem />
+          <DataItem />
+          <DataItem />
+          <DataItem />
+          <DataItem />
+          <DataItem />
+          <DataItem />
+        </div>
+      </div>
+    </div>
+  )
 }
